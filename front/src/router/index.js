a24e4b8cdc7ed4c8d83eb8ac6a43b04b93848f24
Antonio Hofmeister <antonio.hofmeister@superuber.com>
2018-12-11 01:02:57 -0200

    new vue router explore route

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -23,6 +23,18 @@ const router = new Router({
       }
     },
     {
+      path: '/explore',
+      component: load('components/views/Explore'),
+      name: 'explore',
+      meta: {
+        auth: false,
+        keepAlive: false,
+        top: {
+          show: true
+        }
+      }
+    },
+    {
       path: '*',
       component: load('components/home/HomePage'),
       name: 404,
