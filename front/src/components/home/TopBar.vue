1df550440109089066637cb22466fbb4998e09f6
Antonio Hofmeister <antonio.hofmeister@superuber.com>
2018-12-11 01:04:07 -0200

    adjust top bar + dispatch get GPS portion on router click

--- front/src/components/home/TopBar.vue
+++ front/src/components/home/TopBar.vue
@@ -2,20 +2,20 @@
   <transition name="fade">
     <header class="topbar" v-if="getMeta">
       <div class="wrap">
-        <a class="back" v-if='getMeta.back' @click="goBack">
+        <a class="wrap_back" v-if='getMeta.back' @click="goBack">
           <img src="static/icons/backarrow-icon.svg">
         </a>
-        <router-link class="logo" v-if='!getMeta.back' to="/" tag="a">
+        <router-link class="wrap_logo" v-if='!getMeta.back' to="/" tag="a">
         LOGO
         </router-link>
         <div class="controls">
           <div
             class="controls_btn"
           >Create</div>
-          <div
+          <div @click="onClickExplore()"
             class="controls_btn"
           >Explore</div>
-          <div
+          <div @click="$router.push('/')"
             class="controls_btn"
           >Home</div>
         </div>
@@ -64,7 +64,14 @@ export default {
   methods: {
     goBack: function () {
       this.$router.push(-1)
-    }
+    },
+    onClickExplore (evt) {
+      this.getCurrentPosition()
+      this.$router.push('/explore')
+    },
+    ...mapActions('map', [
+      'getCurrentPosition'
+    ])
   },
   watch: {
   }
@@ -77,23 +84,27 @@ $BtnSizeResetAuto: auto !important
 $BtnSizeResetNone: none !important
 
 .topbar
+  position: fixed
+  width: 100%
   padding-right: 30px
   padding-left: 30px
   background: $headerBar
+  z-index: 10
   height: 50px
   .wrap
     justify-content: space-between
     display: flex
     flex-direction: row
     width: 100%
-    .logo
+    &_logo
+      justify-content: center
+      align-self: center
       width: 50px
-      height: 50px
       color: white
       img
         max-width: 100%
         height: auto
-    .back
+    &_back
       color: white
       width: 15px
       padding: 2px
@@ -102,17 +113,18 @@ $BtnSizeResetNone: none !important
       width: 50%
       height: 50px
       display: flex
-      align-content: right !important
-      justify-content: right !important
-      flex-direction: row-reverse !important
+      align-content: right
+      justify-content: right
+      flex-direction: row-reverse
       &_btn
+        cursor: pointer
         width: 100px
         height: 100%
         margin-left: 5px
         color: white
         display: flex
-        justify-content: center !important
-        flex-direction: column !important
+        justify-content: center
+        flex-direction: column
         text-align: center
 
 .btn_topbar
