9d5d67277cef6425664262d3d5b18e5dd09ab950
Antonio Hofmeister <antonio.hofmeister@superuber.com>
2018-12-18 00:57:16 -0200

    start developing podcast pills in Map

---
+++ front/src/components/molecules/PodcastPill.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="pill">
+    TODO: Slideshow. Go to story page to listen to podcast chapters.
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  props: {
+  },
+  methods: {
+  }
+
+}
+</script>
+<style lang="sass" scoped>
+@import '../../assets/sass/podcasts'
+.pill
+  background: red
+  height: 100px
+  width: 200px
+</style>
